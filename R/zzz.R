9781bf3ea4fa8930316b45f65696098e7c45182d
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-01-05 08:03:40 +0100

    Load only if python package is installed

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
-.onLoad <- function(libname, pkgname) {
+load_pharmpy <- function() {
     env <- parent.env(environment())
-    pharmpy <- reticulate::import("pharmpy")
     modeling <- reticulate::import("pharmpy.modeling")
 
     # Assign all functions in pharmpy.modeling to the parent environment
@@ -10,5 +9,13 @@
     }
 
     pharmpy_results <- reticulate::import("pharmpy.results")
-    read_results <- pharmpy_results$read_results
+    assign("read_results", pharmpy_results$read_results, env)
+}
+
+
+.onLoad <- function(libname, pkgname) {
+    if (reticulate::py_module_available("pharmpy")) {
+        reticulate::import("pharmpy")
+        load_pharmpy()
+    }
 }
