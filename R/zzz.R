de1cd61bcde451749f02bb6d7f37b3bddf24516a
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-01-15 17:07:58 +0100

    Add documentation

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,17 @@
-load_pharmpy <- function() {
-    env <- parent.env(environment())
-    modeling <- reticulate::import("pharmpy.modeling")
-
-    # Assign all functions in pharmpy.modeling to the parent environment
-    funcs <- modeling$`__all__`
-    for (func in funcs) {
-        assign(func, modeling[[func]], env)
-    }
-
-    pharmpy_results <- reticulate::import("pharmpy.results")
-    assign("read_results", pharmpy_results$read_results, env)
-}
-
-
 .onLoad <- function(libname, pkgname) {
     if (reticulate::py_module_available("pharmpy")) {
         reticulate::import("pharmpy")
-        load_pharmpy()
+
+        env <- parent.env(environment())
+        modeling <- reticulate::import("pharmpy.modeling")
+
+        # Assign all functions in pharmpy.modeling to the parent environment
+        funcs <- modeling$`__all__`
+        for (func in funcs) {
+            assign(func, modeling[[func]], env)
+        }
+
+        pharmpy_results <- reticulate::import("pharmpy.results")
+        assign("read_results", pharmpy_results$read_results, env)
     }
 }
