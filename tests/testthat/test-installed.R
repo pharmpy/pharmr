9bf6177b8f71c256ed5bdba593a7e1d3eefed2a3
Stella <stella.belin@farmaci.uu.se>
2021-03-15 08:51:43 +0100

    Test that model exists

--- tests/testthat/test-installed.R
+++ tests/testthat/test-installed.R
@@ -8,4 +8,5 @@ test_that("pharmr is installed", {
     library(pharmr)
 
     model <- read_model_from_string("$PROBLEM base model\n$INPUT ID DV TIME\n$DATA file.csv IGNORE=@\n$PRED\nY=THETA(1)\n$THETA 0.1\n$EST METHOD=1\n")
+    expect_error(model, NA)
 })
