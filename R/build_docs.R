e14b08d92b0416020a3e6419dbb16253b1fa9304
Stella <stella.belin@farmaci.uu.se>
2021-03-15 09:02:39 +0100

    Script to build Python documentation

---
+++ R/build_docs.R
@@ -0,0 +1,68 @@
+create_docs <- function() {
+  py_capture_output <- reticulate:::py_capture_output
+  import_builtins <- reticulate::import_builtins
+  
+  reticulate::import("pharmpy")
+  modeling <- reticulate::import("pharmpy.modeling")
+  
+  funcs <- modeling$`__all__`
+  
+  for (func in funcs) {
+    py_doc <- py_capture_output(import_builtins()$help(get(func)), type = "stdout")
+    py_doc <- unlist(strsplit(py_doc, split='\n'))[-1]
+    py_doc <- lapply(py_doc, trimws)
+    py_doc <- py_doc[py_doc != '']
+
+    arguments <- c('')
+    value <- c('')
+    description <- py_doc[-1]
+    
+    if ('Parameters' %in% py_doc) {
+      parameters_start_index <- match('Parameters', py_doc)
+      arguments <- py_doc[-1:-(parameters_start_index + 1)]
+      description <- py_doc[2:(parameters_start_index - 1)]
+      if ('Returns' %in% py_doc) {
+        returns_start_index <- match('Returns', py_doc)
+        value <- py_doc[-1:-(returns_start_index + 1)]
+        arguments <- arguments[1:(length(arguments) - length(value) - 2)]
+      }
+      arguments <- split_to_items(arguments)
+    }
+    
+    description <- paste(description, collapse = '\n\n')
+    
+    value <- paste(value, collapse = '\n\n')
+    file_name <- paste(getwd(), '/man/', func, '.Rd', sep = '')
+    
+    doc_link <- 'https://pharmpy.github.io/latest/reference/pharmpy.modeling.html?highlight=add_cov#pharmpy.modeling.'
+    
+    rd_content <- paste('\\name{', func, '}\n',
+                        '\\alias{', func, '}\n',
+                        '\\title{', func, '}\n',
+                        '\\description{\n', 
+                        '\\href{', doc_link, func, '}{Link to Python API reference} (e.g. for correct rendering of equations)\n\n',
+                        description, '\n}\n', 
+                        '\\usage{\n', py_doc[1], '\n}\n',
+                        '\\arguments{\n', arguments, '\n}\n',
+                        '\\value{\n', value, '\n}\n',
+                        sep = '')
+    
+    rd_file <- cat(rd_content, file = file_name)
+  }
+}
+
+split_to_items <- function(arguments_raw) {
+  list_of_items <- grep(' : ', arguments_raw, value = TRUE)
+  arguments <- ''
+  
+  for (def in list_of_items) {
+    split <- unlist(strsplit(def, ':'))
+    item <- trimws(split[1])
+    type <- trimws(split[2])
+    
+    definition_index <- match(def, arguments_raw) + 1
+    definition <- paste(type, trimws(arguments_raw[definition_index]), sep = ', ')
+    arguments <- paste(arguments, '\\item{', item, '}{', definition, '}', sep = '\n')
+  }
+  return(arguments)
+}
