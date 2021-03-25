0a6415af130cfb3a21338b1d05a8d0f9f0ef9bf0
Stella <stella.belin@farmaci.uu.se>
2021-03-25 11:37:05 +0100

    Fix how command line arguments are parsed

--- deploy/update_desciption.R
+++ deploy/update_desciption.R
@@ -2,9 +2,13 @@ update_description <- function(release_version='minor') {
   desc::desc_bump_version(release_version)
   desc::desc_set('Date', Sys.Date())
 }
-
 library(desc)
 library(here)
 
-release_version <- commandArgs()
-update_description(release_version)
+release_version <- commandArgs(trailingOnly = TRUE)
+
+if (length(release_version) > 0) {
+  update_description(release_version)
+} else {
+  update_description()
+}
