cef2b3109762bc756195fab7681a3ad2855a0d40
Stella <stella.belin@farmaci.uu.se>
2021-04-07 09:11:14 +0200

    Use Pharmpy version for Pharmr-versioning

--- deploy/update_description.R
+++ deploy/update_description.R
@@ -1,14 +1,17 @@
 update_description <- function(release_version='minor') {
-  desc::desc_bump_version(release_version)
+  pharmpy <- reticulate::import("pharmpy")
+  pharmpy_version <- pharmpy$`__version__`
+  
+  description <- paste('An R wrapper for the Pharmpy pharmacometrics library via the reticulate package. ',
+                       'The Pharmpy documentation was automatically generated from Pharmpy version ', 
+                       pharmpy_version, '.', sep='')
+  
+  desc::desc_set('Version', pharmpy_version)
   desc::desc_set('Date', Sys.Date())
+  desc::desc_set('Description', description)
 }
 library(desc)
 library(here)
+library(pharmr)
 
-release_version <- commandArgs(trailingOnly = TRUE)
-
-if (length(release_version) > 0) {
-  update_description(release_version)
-} else {
-  update_description()
-}
+update_description()
