284955ceba204c52f27ef4a242dd445c8b838964
Stella <stella.belin@farmaci.uu.se>
2021-04-23 14:13:06 +0200

    Use delay load

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onLoad <- function(libname, pkgname) {
     if (reticulate::py_module_available("pharmpy")) {
         check_pharmpy()
-        reticulate::import("pharmpy")
+        reticulate::import("pharmpy", delay_load=TRUE)
 
         env <- parent.env(environment())
-        modeling <- reticulate::import("pharmpy.modeling")
+        modeling <- reticulate::import("pharmpy.modeling", delay_load=TRUE)
 
         # Assign all functions in pharmpy.modeling to the parent environment
         funcs <- modeling$`__all__`
