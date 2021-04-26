7b58a6b6d480d659008c845e9894963ee0640f73
Stella <stella.belin@farmaci.uu.se>
2021-04-26 14:10:55 +0200

    Only load pharmpy once

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,13 @@
 .onLoad <- function(libname, pkgname) {
     if (reticulate::py_module_available("pharmpy")) {
-        check_pharmpy()
-        reticulate::import("pharmpy", delay_load=TRUE)
-
+        pharmpy <- reticulate::import("pharmpy", delay_load=TRUE)
+        check_pharmpy(pharmpy$`__version__`)
         env <- parent.env(environment())
-        modeling <- reticulate::import("pharmpy.modeling", delay_load=TRUE)
 
         # Assign all functions in pharmpy.modeling to the parent environment
-        funcs <- modeling$`__all__`
+        funcs <- pharmpy$modeling$`__all__`
         for (func in funcs) {
-            assign(func, modeling[[func]], env)
+            assign(func, pharmpy$modeling[[func]], env)
         }
     }
 }
