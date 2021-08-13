42da1315dfb335ddbfaa9bd1f2ff52df75c76a21
Stella <stella.belin@farmaci.uu.se>
2021-08-13 10:51:53 +0200

    Use on_error to avoid issues with loading pharmpy on systems with multiple envs

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,23 @@
+on_load_pharmpy <- function() {
+    check_pharmpy(pharmpy$`__version__`)
+    env <- parent.env(environment())
+    
+    # Assign all functions in pharmpy.modeling to the parent environment
+    funcs <- pharmpy$modeling$`__all__`
+    for (func in funcs) {
+        assign(func, pharmpy$modeling[[func]], env)
+    }
+}
+
+on_error_pharmpy <- function() {
+    message("Error importing pharmpy")
+    message("Check if pharmpy is installed")
+}
+
+pharmpy <- NULL
+
 .onLoad <- function(libname, pkgname) {
     if (reticulate::py_module_available("pharmpy")) {
-        pharmpy <- reticulate::import("pharmpy", delay_load=TRUE)
-        check_pharmpy(pharmpy$`__version__`)
-        env <- parent.env(environment())
-
-        # Assign all functions in pharmpy.modeling to the parent environment
-        funcs <- pharmpy$modeling$`__all__`
-        for (func in funcs) {
-            assign(func, pharmpy$modeling[[func]], env)
-        }
+        pharmpy <<- reticulate::import("pharmpy", delay_load=list(on_load=on_load_pharmpy, on_error=on_error_pharmpy))
     }
 }
