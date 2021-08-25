0570a7822d62d217d05ac149c73beef94dd6196d
Stella <stella.belin@farmaci.uu.se>
2021-08-25 15:43:56 +0200

    Add roxygen doc for install and reset index, export to namespace

--- R/install.R
+++ R/install.R
@@ -1,3 +1,10 @@
+#' @title
+#' Install Pharmpy
+#' 
+#' @description
+#' Install the pharmpy-core python package into the r-reticulate virtual environment
+#' 
+#' @export
 install_pharmpy <- function() {
     reticulate::py_install("pharmpy-core", pip=T)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,12 @@
+#' @title
+#' Reset index
+#' 
+#' @description
+#' Reset index of dataframe. 
+#' 
+#' Reset index from a multi indexed data.frame so that index is added as columns
+#' @param df A data.frame converted from python using reticulate
+#' @export
 reset_index <- function(df) {
     ind <- attributes(df)$pandas.index
     inddf <- suppressWarnings(ind$to_frame(index=FALSE))
