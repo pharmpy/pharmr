26daeee2c1c83ba391888b30e8935d56f57c9a1d
Stella <stella.belin@farmaci.uu.se>
2021-08-25 15:59:06 +0200

    Change function name for transform

--- tests/testthat/test-transform.R
+++ tests/testthat/test-transform.R
@@ -6,7 +6,7 @@ test_that("pharmr can use pharmpy transformations", {
   skip_on_cran()
   
   model <- read_model('pheno.mod')
-  add_parameter(model, 'MAT')
+  add_individual_parameter(model, 'MAT')
   update_source(model)
   
   expect_match(toString(model), 'POP_MAT')
