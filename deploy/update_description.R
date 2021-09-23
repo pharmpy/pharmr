b207179f980669dc9e11720d8b244d6b9ab384eb
Stella <stella.belin@farmaci.uu.se>
2021-09-23 11:27:33 +0200

    Update DESCRIPTION

--- deploy/update_description.R
+++ deploy/update_description.R
@@ -2,9 +2,9 @@ update_description <- function(release_version='minor') {
   pharmpy <- reticulate::import("pharmpy")
   pharmpy_version <- pharmpy$`__version__`
   
-  description <- paste('An R wrapper for the Pharmpy pharmacometrics library via the reticulate package. ',
-                       'The Pharmpy documentation was automatically generated from Pharmpy version ', 
-                       pharmpy_version, '.', sep='')
+  description <- paste("An R wrapper for the 'Pharmpy' 'pharmacometrics' library via the 'reticulate package'.",  
+                       "The 'Pharmpy' documentation was automatically generated from 'Pharmpy' version", 
+                       pharmpy_version, ".", sep=" ")
   
   desc::desc_set('Version', pharmpy_version)
   desc::desc_set('Date', Sys.Date())
