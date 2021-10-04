2a859618a61e90c18ccddd1ca357d15f37ed2b2e
Stella <stella.belin@farmaci.uu.se>
2021-10-04 15:41:14 +0200

    More options for installing pharmpy

--- R/install.R
+++ R/install.R
@@ -2,9 +2,12 @@
 #' Install Pharmpy
 #' 
 #' @description
-#' Install the pharmpy-core python package into the r-reticulate virtual environment
+#' Install the pharmpy-core python package into virtual environment.
+#' 
+#' @param envname (str) name of environment. Default is r-reticulate
+#' @param method (str) type of environment type (virtualenv, conda). Default is auto (virtualenv is not available on Windows)
 #' 
 #' @export
-install_pharmpy <- function() {
+install_pharmpy <- function(envname='r-reticulate', method='auto') {
     reticulate::py_install("pharmpy-core", pip=T)
 }
