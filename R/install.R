0b8ed7eebded8a0f0af82bced615cbfc9fe61018
Stella <stella.belin@farmaci.uu.se>
2022-06-03 11:39:11 +0200

    Use current pharmr version as version of Pharmpy to install

--- R/install.R
+++ R/install.R
@@ -2,12 +2,15 @@
 #' Install Pharmpy
 #' 
 #' @description
-#' Install the pharmpy-core python package into virtual environment.
+#' Install the pharmpy-core python package into virtual environment. Uses the same Pharmpy version as pharmr.
 #' 
 #' @param envname (str) name of environment. Default is r-reticulate
 #' @param method (str) type of environment type (virtualenv, conda). Default is auto (virtualenv is not available on Windows)
 #' 
+#' @importFrom utils packageVersion
 #' @export
 install_pharmpy <- function(envname='r-reticulate', method='auto') {
-    reticulate::py_install("pharmpy-core", envname=envname, method=method, pip=T)
+    version <- packageVersion('pharmr')
+    pharmpy_to_install <- paste('pharmpy-core', version, sep='==')
+    reticulate::py_install(pharmpy_to_install, envname=envname, method=method, pip=T)
 }
