46be246daff2d434ce2fe1adf187bf55f0aaf36a
Stella <stella.belin@farmaci.uu.se>
2022-06-09 08:12:17 +0200

    Add option to specify Pharmpy version

--- R/install.R
+++ R/install.R
@@ -6,11 +6,19 @@
 #' 
 #' @param envname (str) name of environment. Default is r-reticulate
 #' @param method (str) type of environment type (virtualenv, conda). Default is auto (virtualenv is not available on Windows)
+#' @param version (str) which pharmpy version to use (use 'same' for most cases)
 #' 
 #' @importFrom utils packageVersion
 #' @export
-install_pharmpy <- function(envname='r-reticulate', method='auto') {
-    version <- packageVersion('pharmr')
+install_pharmpy <- function(envname='r-reticulate', method='auto', version='same') {
+  if (version == 'latest') {
+    pharmpy_to_install <- 'pharmpy-core'
+  }
+  else { 
+    if (version == 'same') {
+      version <- packageVersion('pharmr')
+    }
     pharmpy_to_install <- paste('pharmpy-core', version, sep='==')
-    reticulate::py_install(pharmpy_to_install, envname=envname, method=method, pip=T)
+  }
+  reticulate::py_install(pharmpy_to_install, envname=envname, method=method, pip=T)
 }
