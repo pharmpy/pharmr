eea4ea5ab57cec466ecf6360f987a002286fe425
Stella <stella.belin@farmaci.uu.se>
2022-06-10 11:20:25 +0200

    Add hidden function install_pharmpy_devel to specify Pharmpy version for install

--- R/install.R
+++ R/install.R
@@ -6,11 +6,25 @@
 #' 
 #' @param envname (str) name of environment. Default is r-reticulate
 #' @param method (str) type of environment type (virtualenv, conda). Default is auto (virtualenv is not available on Windows)
+#' 
+#' @export
+install_pharmpy <- function(envname='r-reticulate', method='auto') {
+  pharmr:::install_pharmpy_devel(envname = envname, method = method, version = 'same')
+}
+
+
+#' @title
+#' Install Pharmpy (with specified version)
+#' 
+#' @description
+#' Install the pharmpy-core python package into virtual environment. 
+#' 
+#' @param envname (str) name of environment. Default is r-reticulate
+#' @param method (str) type of environment type (virtualenv, conda). Default is auto (virtualenv is not available on Windows)
 #' @param version (str) which pharmpy version to use (use 'same' for most cases)
 #' 
 #' @importFrom utils packageVersion
-#' @export
-install_pharmpy <- function(envname='r-reticulate', method='auto', version='same') {
+install_pharmpy_devel <- function(envname='r-reticulate', method='auto', version='same') {
   if (version == 'latest') {
     pharmpy_to_install <- 'pharmpy-core'
   }
@@ -22,3 +36,4 @@ install_pharmpy <- function(envname='r-reticulate', method='auto', version='same
   }
   reticulate::py_install(pharmpy_to_install, envname=envname, method=method, pip=T)
 }
+
