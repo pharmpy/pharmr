a8d2f30f1feac0c26883bb90879038bb92e5383e
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-10-20 13:38:48 +0200

    Fix broken printing functions after move to pharmpy.model

--- R/printing.R
+++ R/printing.R
@@ -1,57 +1,68 @@
 # The knit_print functions are used by knitr in rmarkdown
 # The repr_html functions are used in jupyter
 
-#' @exportS3Method knitr::knit_print pharmpy.statements.Assignment
-knit_print.pharmpy.statements.Assignment <- function(x, ...) {
+#' @exportS3Method knitr::knit_print pharmpy.model.statements.Assignment
+knit_print.pharmpy.model.statements.Assignment <- function(x, ...) {
     res <- x$`_repr_latex_`()
     structure(res, class='knit_asis')
 }
 
-#' @exportS3Method knitr::knit_print pharmpy.statements.ModelStatements
-knit_print.pharmpy.statements.ModelStatements <- function(x, ...) {
+#' @exportS3Method knitr::knit_print pharmpy.model.statements.Statements
+knit_print.pharmpy.model.statements.Statements <- function(x, ...) {
     res <- x$`_repr_html_`()
     structure(res, class='knit_asis')
 }
 
-#' @exportS3Method knitr::knit_print pharmpy.random_variables.RandomVariable
-knit_print.pharmpy.random_variables.RandomVariable <- function(x, ...) {
+#' @exportS3Method knitr::knit_print pharmpy.model.distributions.symbolic.NormalDistribution
+knit_print.pharmpy.model.distributions.symbolic.NormalDistribution <- function(x, ...) {
     res <- x$`_repr_latex_`()
     structure(res, class='knit_asis')
 }
 
-#' @exportS3Method knitr::knit_print pharmpy.random_variables.RandomVariables
-knit_print.pharmpy.random_variables.RandomVariables <- function(x, ...) {
+#' @exportS3Method knitr::knit_print pharmpy.model.distributions.symbolic.JointNormalDistribution
+knit_print.pharmpy.model.distributions.symbolic.JointNormalDistribution <- function(x, ...) {
     res <- x$`_repr_latex_`()
     structure(res, class='knit_asis')
 }
 
-#' @exportS3Method knitr::knit_print pharmpy.statements.ExplicitODESystem
-knit_print.pharmpy.statements.ExplicitODESystem <- function(x, ...) {
+#' @exportS3Method knitr::knit_print pharmpy.model.random_variables.RandomVariables
+knit_print.pharmpy.random_variables.model.RandomVariables <- function(x, ...) {
     res <- x$`_repr_latex_`()
     structure(res, class='knit_asis')
 }
 
-#' @exportS3Method repr::repr_latex pharmpy.statements.Assignment
-repr_latex.pharmpy.statements.Assignment <- function(obj, ...) {
+#' @exportS3Method knitr::knit_print pharmpy.model.statements.ExplicitODESystem
+knit_print.pharmpy.model.statements.ExplicitODESystem <- function(x, ...) {
+    res <- x$`_repr_latex_`()
+    structure(res, class='knit_asis')
+}
+
+#' @exportS3Method repr::repr_latex pharmpy.model.statements.Assignment
+repr_latex.pharmpy.model.statements.Assignment <- function(obj, ...) {
     obj$`_repr_latex_`()
 }
 
-#' @exportS3Method repr::repr_html pharmpy.statements.ModelStatements
-repr_html.pharmpy.statements.ModelStatements <- function(obj, ...) {
+#' @exportS3Method repr::repr_html pharmpy.model.statements.Statements
+repr_html.pharmpy.model.statements.Statements <- function(obj, ...) {
     obj$`_repr_html_`()
 }
 
-#' @exportS3Method repr::repr_latex pharmpy.statements.ExplicitODESystem
-repr_latex.pharmpy.statements.ExplicitODESystem <- function(obj, ...) {
+#' @exportS3Method repr::repr_latex pharmpy.model.statements.ExplicitODESystem
+repr_latex.pharmpy.model.statements.ExplicitODESystem <- function(obj, ...) {
     obj$`_repr_latex_`()
 }
 
-#' @exportS3Method repr::repr_latex pharmpy.random_variables.RandomVariable
-repr_latex.pharmpy.random_variables.RandomVariable <- function(obj, ...) {
+#' @exportS3Method repr::repr_latex pharmpy.model.distributions.symbolic.NormalDistribution
+repr_latex.pharmpy.model.distributions.symbolic.NormalDistribution <- function(obj, ...) {
     obj$`_repr_latex_`()
 }
 
-#' @exportS3Method repr::repr_latex pharmpy.random_variables.RandomVariables
-repr_latex.pharmpy.random_variables.RandomVariables <- function(obj, ...) {
+#' @exportS3Method repr::repr_latex pharmpy.model.distributions.symbolic.JointNormalDistribution
+repr_latex.pharmpy.model.distributions.symbolic.JointNormalDistribution <- function(obj, ...) {
+    obj$`_repr_latex_`()
+}
+
+#' @exportS3Method repr::repr_latex pharmpy.model.random_variables.RandomVariables
+repr_latex.pharmpy.model.random_variables.RandomVariables <- function(obj, ...) {
     obj$`_repr_latex_`()
 }
