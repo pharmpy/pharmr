a80eec547e428ac014379ba6b83e6421c439c886
Stella <stella.belin@farmaci.uu.se>
2022-11-24 09:51:16 +0100

    Add preprocessing function which adds type conversion (e.g. for ints and arrays) (only for type hinted functions)

--- deploy/func_conversion.py
+++ deploy/func_conversion.py
@@ -2,6 +2,7 @@ import inspect
 
 import re
 import textwrap
+from typing import get_args, get_origin, get_type_hints, Union
 
 
 from help_functions import py_to_r_arg
@@ -19,28 +20,28 @@ def create_r_func(func, module):
     if not inspect.getdoc(func):
         raise ValueError(f'No documentation available for {func_name}')
 
-    wrapper_arg_str, pyfunc_arg_str = _get_args(func)
+    params = inspect.signature(func).parameters
+    wrapper_arg_str, pyfunc_arg_str = _get_args(params)
 
     func_def = f'{func_name} <- function({wrapper_arg_str})'
     func_execute = f'func_out <- pharmpy${module_name}${func_name}({pyfunc_arg_str})'
 
     if module_name == 'tools':
-        r_execute = _create_tool_execute(func, func_execute)
+        r_func_body = _create_func_body_tool(func, func_execute)
     else:
-        r_execute = _create_modeling_execute(func, func_execute)
+        r_func_body = _create_func_body_modeling(func, func_execute)
 
     r_wrapper = [f'{func_def} {{',
-                 *r_execute,
+                 *r_func_body,
                  '}']
 
     r_wrapper_indented = _indent(r_wrapper)
     return '\n'.join(r_wrapper_indented)
 
 
-def _get_args(func):
-    sig = inspect.signature(func)
+def _get_args(params):
     wrapper_args, pyfunc_args = [], []
-    for param in sig.parameters.values():
+    for param in params.values():
         if param.kind == param.VAR_KEYWORD or param.kind == param.VAR_POSITIONAL:
             if '...' not in wrapper_args:
                 wrapper_args.append('...'), pyfunc_args.append('...')
@@ -53,30 +54,29 @@ def _get_args(func):
     return ', '.join(wrapper_args), ', '.join(pyfunc_args)
 
 
-def _create_modeling_execute(func, func_execute):
-    func_return = _create_func_return(func)
+def _create_func_body_modeling(func, func_execute):
+    r_func_body = [*_preprocess_input(func),
+                   f'{func_execute}',
+                   *_create_func_return(func)]
+    return r_func_body
 
-    r_execute = [f'{func_execute}',
-                 *func_return]
-    return r_execute
 
-
-def _create_tool_execute(func, func_execute):
+def _create_func_body_tool(func, func_execute):
     error_msg = [
         'message(cond)',
-        'message("Full stack:")',
+        'message(\'Full stack:\')',
         'message(reticulate::py_last_error())',
         'message("pharmr version: ", packageVersion("pharmr"))',
         'message("Pharmpy version: ", print_pharmpy_version())',
         'return(NA)'
     ]
 
-    func_return = _create_func_return(func)
-    r_execute = [
+    r_func_body = [
         'tryCatch(',
         '{',
+        *_preprocess_input(func),
         f'{func_execute}',
-        *func_return,
+        *_create_func_return(func),
         '},',
         'error=function(cond) {',
         *error_msg,
@@ -87,7 +87,52 @@ def _create_tool_execute(func, func_execute):
         ')',
     ]
 
-    return r_execute
+    return r_func_body
+
+
+# FIXME make more general (handle nested types), combine overlapping functionality with
+#  equivalent in docs_conversion.py::_translate_type_hints
+def _preprocess_input(func):
+    try:
+        type_hints = {key: value for key, value in get_type_hints(func).items() if key != 'return'}
+    except AttributeError:
+        type_hints = None
+
+    if not type_hints:
+        return []
+
+    r_preprocess = []
+    for key, value in type_hints.items():
+        args, origin = get_args(value), get_origin(value)
+        if value is Union or origin is Union:
+            r_conversion = _get_conversion_str(key, args, origin)
+        else:
+            r_conversion = _get_conversion_str(key, value, origin)
+
+        if r_conversion is None:
+            continue
+
+        if origin is Union and type(None) in get_args(value):
+            r_conversion_null = [f'if (!is.null({key}) {{',
+                                 r_conversion,
+                                 '}']
+            r_preprocess.extend(r_conversion_null)
+        else:
+            r_preprocess.append(r_conversion)
+
+    return r_preprocess
+
+
+def _get_conversion_str(key, args, origin):
+    if args is int:
+        return f'{key} = as.integer({key})'
+    elif origin is list:
+        return f'{key} = as.list({key})'
+    # FIXME make more general (handles optional type)
+    elif isinstance(args, tuple) and len(args) == 2 and type(None) in args:
+        if get_origin(args[0]) is list:
+            return f'{key} = as.list({key})'
+    return None
 
 
 def _create_func_return(func):
