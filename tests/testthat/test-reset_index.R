db0cea46903c41bcaa117385c6bfca5016a698f4
Stella <stella.belin@farmaci.uu.se>
2022-11-29 11:34:09 +0100

    Test that the index have been resetted for results object

--- tests/testthat/test-reset_index.R
+++ tests/testthat/test-reset_index.R
@@ -6,7 +6,7 @@ test_that("multi index can be reset", {
   skip_on_cran()
   
   res <- read_results('results.json')
-  df <- reset_index(res$covariate_effects)
+  df <- res$covariate_effects
 
   expect_equal(names(df), c("parameter", "covariate", "condition", "p5", "mean", "p95"))
 })
