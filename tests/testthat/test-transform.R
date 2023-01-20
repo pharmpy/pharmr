4881091ea2d5ae937a87cfa7d08ff722dfaf4420
Stella <stella.belin@farmaci.uu.se>
2023-01-20 11:43:07 +0100

    Add tests for some transformations

--- tests/testthat/test-transform.R
+++ tests/testthat/test-transform.R
@@ -5,8 +5,21 @@ test_that("pharmr can use pharmpy transformations", {
   # requires Python
   skip_on_cran()
 
-  model <- load_example_model('pheno')
-  model <- add_individual_parameter(model, 'MAT')
+  model_start <- load_example_model('pheno')
 
-  expect_match(model$model_code, 'POP_MAT')
+  model_mat <- add_individual_parameter(model_start, 'MAT')
+  expect_match(model_mat$model_code, 'POP_MAT')
+
+  # FIXME: Change when results are separate from model object
+  pe <- model_start$modelfit_results$parameter_estimates
+  model_update <- update_inits(model_start, pe)
+  expect_equal(model_start$model_code, model_start$model_code)
+  expect_match(model_update$model_code, '0.00469555) ; PTVCL')
+  expect_no_match(model_start$model_code, '0.00469555) ; PTVCL')
+
+  individual_parameter_statistics <- calculate_individual_parameter_statistics(model_start, "K=CL/V", pe)
+
+  colnames <- names(individual_parameter_statistics)
+  colnames_ref <- c('parameter', 'covariates', 'mean', 'variance', 'stderr')
+  expect_equal(colnames, colnames_ref)
 })
