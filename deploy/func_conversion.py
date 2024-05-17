a823454020bf1e9a23252f662f69d84b88725875
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-05-17 16:54:33 +0200

    Do not give full stacktrace for input validation errors

--- deploy/func_conversion.py
+++ deploy/func_conversion.py
@@ -66,11 +66,17 @@ def _create_func_body_modeling(func, func_execute):
 
 def _create_func_body_tool(func, func_execute):
     error_msg = [
-        'message(cond)',
-        'message(\'Full stack:\')',
-        'message(reticulate::py_last_error())',
-        'message("pharmr version: ", packageVersion("pharmr"))',
-        'message("Pharmpy version: ", print_pharmpy_version())',
+        'err <- reticulate::py_last_error()'
+        'if (err$type == "InputValidationError") {',
+        '    message(err$value)',
+        '} else {',
+        '    message(\'Full stack:\')',
+        '    message(cond)',
+        '    message(err)',
+        '    message("pharmr version: ", packageVersion("pharmr"))',
+        '    message("Pharmpy version: ", print_pharmpy_version())',
+        '    message("This is a BUG. Please report it at https://github.com/pharmpy/pharmpy/issues. Thanks!")'
+        '}',
         'return(NA)'
     ]
 
