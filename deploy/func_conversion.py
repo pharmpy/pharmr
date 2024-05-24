953f73f1e1f738b4797a9c3f58f6159725e00b7e
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-05-24 12:39:59 +0200

    Handle R errors caused by lazy evaluation in func wrappers properly

--- deploy/func_conversion.py
+++ deploy/func_conversion.py
@@ -67,7 +67,9 @@ def _create_func_body_modeling(func, func_execute):
 def _create_func_body_tool(func, func_execute):
     error_msg = [
         'err <- reticulate::py_last_error()',
-        'if (err$type == "InputValidationError") {',
+        'if (is.null(err)) {',
+        '    message(cond)',
+        '} else if (err$type == "InputValidationError") {',
         '    message(err$value)',
         '} else {',
         '    message(\'Full stack:\')',
