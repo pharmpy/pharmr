b71dec1efc8fce5d433585958c385bd0c6692006
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-10-21 09:18:06 +0200

    obj->x in length overload

--- R/overloads.R
+++ R/overloads.R
@@ -1,4 +1,4 @@
 #' @exportS3Method length pharmpy.model.parameters.Parameters
-length.pharmpy.model.parameters.Parameters <- function(obj) {
-    reticulate::py_len(obj)
+length.pharmpy.model.parameters.Parameters <- function(x) {
+    reticulate::py_len(x)
 }
