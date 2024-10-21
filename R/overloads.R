021a138c745a75326ce0ac6e0275cf3344089603
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-10-21 09:13:38 +0200

    Fix signature of length overload

--- R/overloads.R
+++ R/overloads.R
@@ -1,4 +1,4 @@
 #' @exportS3Method length pharmpy.model.parameters.Parameters
-length.pharmpy.model.parameters.Parameters <- function(obj, ...) {
+length.pharmpy.model.parameters.Parameters <- function(obj) {
     reticulate::py_len(obj)
 }
